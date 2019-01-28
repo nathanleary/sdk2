ec28d606862a6549c32af445ab6ae1b787e16d59
Nathan Leary <nathan-leary@mail.com>
2019-01-28 22:04:02 +1000

    adds

--- sdk.go
+++ sdk.go
@@ -320,7 +320,7 @@ func Run(duk *duktape.Context, input string) (dukValue, error) {
 			inputLength := len(input)
 			b, _ := json.Marshal(input[indexStart+1 : i3])
 			input = input[:indexStart] + string(b) + input[i3+1:]
-			//fmt.Println(input)
+			fmt.Println(input)
 			indexStart = indexStart + (len(input) - inputLength)
 			quoteFound = false
 		} else {
